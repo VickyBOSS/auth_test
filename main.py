5860104188523852a869d00ebefac25559d7e7a1
VickyBOSS <vikaspachar63@gmail.com>
2020-04-13 11:49:33 +0530

    corrected message

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ user_repos = UserRepos() # UserRepo.__init__(user_repos)
 
 user = user_repos.signInWithEmailAndPassword(email, password) # User / None
 if user == None:
-    print("You email or pass is wrong")
+    print("Your email or pass is incorrect !")
 else :
     print("Congratulations ! You are signed in as {}".format(user.name))
